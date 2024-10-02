e52173b7070fcd830869347475f2d97218eaa7c7
Niranjan Fartare <niranjanfartare0@gmail.com>
2024-10-02 17:37:17 +0530

    Create whois.js

---
+++ api/whois.js
@@ -0,0 +1,16 @@
+const whois = require('whois-json');
+
+module.exports = async (req, res) => {
+  const { domain } = req.query;
+
+  if (!domain) {
+    return res.status(400).json({ error: 'Domain parameter is required' });
+  }
+
+  try {
+    const data = await whois(domain);
+    return res.status(200).json({ domain, whois: data });
+  } catch (error) {
+    return res.status(500).json({ error: 'Error performing WHOIS lookup' });
+  }
+};
